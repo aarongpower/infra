fce0fcc1a5fe4684e85867a3cfe607aa3ada6b67
Aaron Power <aarongpower@gmail.com>
2025-03-05 15:11:53 +0000

    add yggdrasil server to nixos-wsl config

--- systems/vulcan-nixos/services.nix
+++ systems/vulcan-nixos/services.nix
@@ -11,6 +11,7 @@
         devices = {
           "astra" = { id = "UZIC2YY-JKLMMEQ-CXJU4PW-QV2CWY3-GPD25DH-AS5GZ4Y-QWVUMO7-GWUUJAN"; };
           "vulcan" = { id = "XSHVAS4-XDTLBVW-AM7GCF4-NCLP67Y-FZW6XVF-YU46MRJ-ACQFPPP-AJLFKQ6"; };
+          "yggdrasil" = { id = "YJEIWH4-XO465X4-FXQRPQ5-MTS5ZS2-Z3VCRZI-Z2KOWU3-KBB4XVJ-OTJUKQW"; };
         };
         folders = {
           # "dev" = {
