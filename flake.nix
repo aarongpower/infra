ed03ace4524fb71dfb32f967917eb608a4355a04
Aaron Power <aarongpower@outlook.com>
2025-04-19 12:18:20 +0700

    update nix-darwin

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
     };
 
     nix-darwin = {
-      url = "github:LnL7/nix-darwin";
+      url = "github:nix-darwin/nix-darwin/nix-darwin-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
