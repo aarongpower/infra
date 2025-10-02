61a2062d28f0e847c74488c53b85aa805c17fd60
Aaron Power <aarongpower@gmail.com>
2025-10-02 13:33:34 +0700

    Deploy to geth on 2025-10-02 13:33:34

--- nix/nixosSystems/geth/gaming.nix
+++ nix/nixosSystems/geth/gaming.nix
@@ -5,12 +5,18 @@
     ];
 
     # Enable 32-bit support (required for Steam)
-    hardware.opengl.driSupport32Bit = true;
-    hardware.pulseaudio.support32Bit = true; # If using PulseAudio
+    # hardware.opengl.driSupport32Bit = true;
+    # hardware.pulseaudio.support32Bit = true; # If using PulseAudio
 
     programs.steam = {
       enable = true;
-      remotePlay.openFirewall = true;
-      dedicatedServer.openFirewall = true;
+      # remotePlay.openFirewall = true;
+      # dedicatedServer.openFirewall = true;
+      
+      # # Enable XWayland support for Steam
+      # gamescopeSession.enable = true;
     };
+
+    # Make sure XWayland is enabled
+    programs.xwayland.enable = true;
 }
