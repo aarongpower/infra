e9c3a6fa2684e2be11ed2a769ff6a6fd3f24aee1
Aaron Power <aarongpower@gmail.com>
2025-10-02 15:24:08 +0700

    Deploy to geth on 2025-10-02 15:24:08

--- nix/nixosSystems/geth/configuration.nix
+++ nix/nixosSystems/geth/configuration.nix
@@ -63,6 +63,13 @@ in {
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
+  # programs.niri = {
+  #   package = unstable.niri;
+  #   settings = {
+  #     extraOptions = [ "--xwayland" ];
+  #   };
+  # };
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.aaronp = {
     isNormalUser = true;
