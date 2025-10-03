d0fe213bea2876d5b4538dd0794f2825b7c67462
Aaron Power <aarongpower@gmail.com>
2025-10-03 15:17:54 +0700

    Deploy to geth on 2025-10-03 15:17:54

--- nix/nixosSystems/geth/virtualisation.nix
+++ nix/nixosSystems/geth/virtualisation.nix
@@ -7,23 +7,29 @@
   virtualisation.libvirtd = {
     enable = true;
     qemu = {
-      package = pkgs.qemu_kvm;
+      package = pkgs.qemu_full;   # <-- switch from qemu_kvm to qemu_full
       ovmf.enable = true;
       swtpm.enable = true;
-      runAsRoot = false; # recommended for desktop usage
-      vhostUserPackages = [pkgs.virglrenderer pkgs.spice-gtk];
+      runAsRoot = false;
     };
   };
+
+  # Ensure Mesa/DRI stack is on (newer NixOS uses hardware.graphics.*)
+  hardware.graphics.enable = true;
+  # (optional but handy) hardware.graphics.enable32Bit = true;
+
   programs.virt-manager.enable = true;
-  users.users.aaronp.extraGroups = lib.mkAfter ["libvirtd" "kvm"];
-  environment.systemPackages = with pkgs;
-    lib.mkAfter [
-      virtio-win
-      virt-manager
-      spice-gtk
-      usbredir
-      virglrenderer
-      mesa
-    ];
+
+  environment.systemPackages = with pkgs; lib.mkAfter [
+    virtio-win
+    virt-manager
+    spice-gtk
+    usbredir
+    virglrenderer
+  ];
+
   virtualisation.spiceUSBRedirection.enable = true;
+
+  users.users.aaronp.extraGroups = lib.mkAfter [ "libvirtd" "kvm" ];
 }
+
